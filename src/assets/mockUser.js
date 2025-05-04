a29d91ffa220240d3221ab0ff7de19743987fdc0
Leen151 <leen.m@orange.fr>
2025-05-04 17:50:26 +0200

    mock users

---
+++ src/assets/mockUser.js
@@ -0,0 +1,35 @@
+export const users = [
+  {
+    "firstName": "Alice",
+    "lastName": "Dupont",
+    "dateOfBirth": "06/15/1985",
+    "startDate": "09/01/2020",
+    "department": "marketing",
+    "street": "1234 Sunset Blvd",
+    "city": "Los Angeles",
+    "state": "California",
+    "zipCode": "90001"
+  },
+  {
+    "firstName": "Bob",
+    "lastName": "Martin",
+    "dateOfBirth": "03/22/1990",
+    "startDate": "04/15/2019",
+    "department": "engineering",
+    "street": "5001 Innovation Drive",
+    "city": "Austin",
+    "state": "Texas",
+    "zipCode": "73301"
+  },
+  {
+    "firstName": "Claire",
+    "lastName": "Lemoine",
+    "dateOfBirth": "11/10/1995",
+    "startDate": "06/23/2022",
+    "department": "human-resources",
+    "street": "350 Park Ave",
+    "city": "New York",
+    "state": "New York",
+    "zipCode": "10156"
+  }
+]
