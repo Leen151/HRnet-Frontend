29455345d5130cf02c90950513feef00accc62a3
Leen151 <leen.m@orange.fr>
2025-05-04 16:42:29 +0200

    ajout de react-select

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,22 +1,31 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import DatePicker from 'react-datepicker'
+import Select from 'react-select';
+import { departments, states } from '../../assets/options'
 import 'react-datepicker/dist/react-datepicker.css'
 import "./form.scss"
 
 export const Form = () => {
   const [selectedBirthDate, setSelectedBirthDate] = useState(null);
   const [selectedStartDate, setSelectedStartDate] = useState(null);
+  const [selectedDepartment, setSelectedDepartment] = useState(null);
+  const [selectedState, setSelectedState] = useState(null);
 
   return (
     <div className="container">
       <h2>Create Employee</h2>
 
       <form action="#" id="create-employee">
-        <label for="first-name">First Name</label>
-        <input type="text" id="first-name" />
-
-        <label for="last-name">Last Name</label>
-        <input type="text" id="last-name" />
+        <div className='name'>
+          <div>
+            <label for="first-name">First Name</label>
+            <input type="text" id="first-name" />
+          </div>
+          <div>
+            <label for="last-name">Last Name</label>
+            <input type="text" id="last-name" />
+          </div>
+        </div>
 
         <label htmlFor="date-of-birth">Date of Birth</label>
         <DatePicker
@@ -41,21 +50,29 @@ export const Form = () => {
           <label for="city">City</label>
           <input id="city" type="text" />
 
-          <label for="state">State</label>
-          <select name="state" id="state"></select>
+          <label htmlFor="state">State</label>
+          <Select
+          id="state"
+          options={states}
+          value={selectedState}
+          onChange={setSelectedState}
+          placeholder="Select a State"
+          classNamePrefix="custom-select"
+          />
 
           <label for="zip-code">Zip Code</label>
           <input id="zip-code" type="number" />
         </fieldset>
 
-        <label for="department">Department</label>
-        <select name="department" id="department">
-          <option>Sales</option>
-          <option>Marketing</option>
-          <option>Engineering</option>
-          <option>Human Resources</option>
-          <option>Legal</option>
-        </select>
+        <label htmlFor="department">Department</label>
+        <Select
+          id="department"
+          options={departments}
+          value={selectedDepartment}
+          onChange={setSelectedDepartment}
+          placeholder="Select a Department"
+          classNamePrefix="custom-select"
+        />
       </form>
 
       <button className="save-form" onclick="saveEmployee()">Save</button>
